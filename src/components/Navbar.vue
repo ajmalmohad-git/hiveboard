413e35c4b7aa86a79a3c634be227e965654e4d5d
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-04-30 19:14:16 +0530

    commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,18 +8,18 @@
         <svg class="fill-current h-3 w-3 " viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </div>
-    <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+    <div :class="open ? 'block': 'hidden'" class="w-full sm:flex sm:items-center sm:w-auto">
       <div class="text-sm sm:flex-grow">
-        <a href="/" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-hiveprimary2 hover:text-hiveaccent2 mr-8">
+        <a href="/" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-hiveprimary2 hover:text-hiveaccent2 sm:mr-12">
           <router-link to="/">Home</router-link>
         </a>
-        <a href="/about" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-hiveprimary2 hover:text-hiveaccent2 mr-8">
+        <a href="/about" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-hiveprimary2 hover:text-hiveaccent2 sm:mr-12">
           <router-link to="/about">About</router-link>
         </a>
-        <a href="/" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-hiveprimary2 hover:text-hiveaccent2 mr-8">
+        <a href="/" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-hiveprimary2 hover:text-hiveaccent2 sm:mr-12">
           <router-link to ="/">Courses</router-link>
         </a>
-        <a href="/" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-hiveprimary2 hover:text-hiveaccent2 mr-8 mb-5 sm:mb-0">
+        <a href="/" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-hiveprimary2 hover:text-hiveaccent2 sm:mr-12 mb-5 sm:mb-0">
           Join
         </a>
       </div>

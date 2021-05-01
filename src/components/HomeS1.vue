12fa2078b51f38f4a07c072450e9b7bd82c1e990
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-05-01 12:18:28 +0530

    commit

--- src/components/HomeS1.vue
+++ src/components/HomeS1.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="section1 h-screen flex flex-col items-start sm:items-center md:flex-row text-left overflow-x-hidden">
-     <div class="flex-1 px-12 md:px-10 lg:px-20 order-2 md:order-1 mt-10 ">
+     <div class="flex-1 px-12 md:px-10 lg:px-20 order-2 md:order-1 mt-16">
        <img class="absolute top-1 left-1 h-8 md:h-10" src="../assets/images/Navlogo.png"/>
        <p class="text-xs text-hiveaccent1 font-bold">GET STARTED</p>
        <h1 class="mt-1 md:mt-0 font-extrabold text-3xl sm:text-4xl lg:text-5xl">Show yourself &amp; </h1>
@@ -30,9 +30,9 @@ export default {
     height:50vh;
   }
 }
-@media (max-width:508px){
+@media (max-width:728px){
   .img1{
-    padding-top:10vh;
+    padding-top:15vh;
   }
 }
 </style>
